5a8ab65130e4e1bf767fadf4a638d6f99cbada0d
ludoff <ludovicofiore25@gmail.com>
2024-06-03 16:59:17 +0200

    abbellimento pagina con elementi html css e completamento esercizio

--- js/main.js
+++ js/main.js
@@ -4,11 +4,23 @@
 // chiedere il nome all'utente 
 let askName = prompt("inserisci qui il tuo nome");
 
+console.log(askName);
+
+document.getElementById("name").innerHTML = askName
+
 // chiedere il cognome all'utente 
 let askSurname = prompt("inserisci qui il tuo cognome");
 
+console.log(askSurname);
+
+document.getElementById("surname").innerHTML = askSurname
+
 // chiedere il colore preferito all'utente
 let askColor = prompt("inserisci qui il tuo colore preferito");
+
+console.log(askColor);
+
+document.getElementById("color").innerHTML = askColor
 
 // inserire il 23
 const insertNumber = "23";
